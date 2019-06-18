41ca397e28f84efb6bfc6c6124307cac42936f80
qin-chen <1769144676@qq.com>
2019-06-18 15:33:23 +0800

    utils 增加get 方法

--- src/main/java/com/qfedu/utils/UploadUtils.java
+++ src/main/java/com/qfedu/utils/UploadUtils.java
@@ -47,6 +47,37 @@ public class UploadUtils {
         return id;
     }*/
 
+    public String getAccessKey() {
+        return accessKey;
+    }
+
+    public void setAccessKey(String accessKey) {
+        this.accessKey = accessKey;
+    }
+
+    public String getSecretKey() {
+        return secretKey;
+    }
+
+    public void setSecretKey(String secretKey) {
+        this.secretKey = secretKey;
+    }
+
+    public String getBucket() {
+        return bucket;
+    }
+
+    public void setBucket(String bucket) {
+        this.bucket = bucket;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
 
     //七牛上传
     public String upload(MultipartFile multipartFile) {
